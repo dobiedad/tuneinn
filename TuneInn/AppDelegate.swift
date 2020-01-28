188a5abe5b873128d92ff4b947c1e5dcac6edd4a
dobiedad <leomdiv@gmail.com>
2020-01-28 12:07:30 +0000

    v1

---
+++ TuneInn/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TuneInn
+//
+//  Created by Leo on 28/01/2020.
+//  Copyright © 2020 Limitless. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TuneInn/ViewController.swift
@@ -0,0 +1,17 @@
+//  ViewController.swift
+
+import Cocoa
+import WebKit
+
+class ViewController: NSViewController {
+    @IBOutlet weak var webView: WKWebView!
+    
+    let url = "https://tunein.com"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let request = URLRequest(url: URL(string: url)!)
+        webView.load(request)
+    }
+}
